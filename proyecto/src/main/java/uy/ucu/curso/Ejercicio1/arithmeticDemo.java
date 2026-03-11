b43a8da099802783474f00d27fa11461d5132b35
Sebastian1891-sys <seba1891silveira@gmail.com>
2026-03-11 10:38:27 -0300

    Eje 2

---
+++ proyecto/src/main/java/uy/ucu/curso/Ejercicio1/arithmeticDemo.java
@@ -0,0 +1,18 @@
+package uy.ucu.curso.Ejercicio1;
+
+public class arithmeticDemo { 
+    public static void main(String[] args) {
+     int result = 1 + 2;// Aca conviene poner 3 de una, osea una asignacion simple.
+        result = result - 1; //Aca una asignacion compuesta, porque se esta usando el mismo resultado para hacer la operacion.
+          result = result * 2;// 
+           result = result / 2;
+            result = result + 8; 
+            result = result % 7; 
+        }
+
+        String texto = "123";
+        int numero = Integer.parseInt(texto);
+
+        
+
+         } 
